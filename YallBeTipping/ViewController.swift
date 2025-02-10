cb0c7e775aa7ea1ccf0ff6308b384eee2c4602c3
Joseph Iroh <cjoseph@quidax.com>
2025-02-10 11:44:40 +0100

    Initial Commit

---
+++ YallBeTipping/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YallBeTipping
+//
+//  Created by Joseph Iroh on 10/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
